09924a4202c9c4a857b81c485cdb797cba502535
Wolfy76700 <wolfy76700@yahoo.fr>
2016-11-24 12:32:50 +0100

    Debug #2

--- MEDEV_TP2_Lesauvage_Thery_Shirato_Marco/seuillage.h
+++ MEDEV_TP2_Lesauvage_Thery_Shirato_Marco/seuillage.h
@@ -7,12 +7,10 @@
 
 #ifndef SEUILLAGE_H
 #define	SEUILLAGE_H
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
 
 
-void seuillage(std::ifstream image, int argseuil, std::string outputname);
+
+void seuillage(std::ifstream & image, int argseuil, std::string outputname);
 
 #endif	/* SEUILLAGE_H */
 
