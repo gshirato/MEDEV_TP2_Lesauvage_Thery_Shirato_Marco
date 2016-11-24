482e841f2bf7591784e66ad61e56f8003ba31571
user <user@user-VirtualBox>
2016-11-24 10:51:05 +0100

    Fonction de seuillage à terminer

--- MEDEV_TP2_Lesauvage_Thery_Shirato_Marco/main.cpp
+++ MEDEV_TP2_Lesauvage_Thery_Shirato_Marco/main.cpp
@@ -14,13 +14,45 @@ using namespace std;
  * 
  */
 
-void seuillage(ifstream image){
+void seuillage(ifstream image, int argseuil, string outputname){//on propose à l'utilisateur
+                                //de mettre son propre argument de seuillage
     string current_line;
+    ofstream outfile(outputname);
     getline(image, current_line);//P2, inutile
+    outfile << current_line <<endl;//on recopie dans le nouveau fichier
     getline(image, current_line);//commentaires, inutiles
+    outfile << current_line <<endl;
     getline(image, current_line);//on a maintenant la largeur puis la hauteur, 
     //qui nous donnerons les bornes de nos boucles for, séparés par un espace
-    
+    outfile << current_line <<endl;
+    string lar;
+    string hau;
+    unsigned int i=0;//entier de parcours
+    unsigned int taille = current_line.size();
+    char j=current_line(i);//caractère de parcours
+    while (i < taille && j!=' '){
+        j=current_line(i);
+        lar.append(j);
+        i++;
+    }
+    i++;
+    while (i<taille){
+        j=current_line(i);
+        hau.append(j);
+        i++;
+    }
+    int largeur = (int) lar;
+    int hauteur = (int) hau;
+    for(int h = 0;h<hauteur;h++){
+        getline(image, current_line);//on se place sur la ligne qu'on va traiter
+        j=current_line[0];//on récupère le premier caractère
+        i=0;
+        while(i<largeur){
+            
+            i++;
+        }
+        
+    }
 }
 
 int main(int argc, char** argv) {
